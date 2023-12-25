07246fb3df8d45c9a6299a076f4adf892bc1c462
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2023-12-25 23:30:50 +0700

    up

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,8 @@
+import "../src/sytle/main.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ my-react-app/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboeard() {
+  return (
+    <div>
+      <h1>Dashboeard.</h1>
+    </div>
+  );
+}
+
+export default Dashboeard;
